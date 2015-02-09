6a8ac524ca2b49818d981ffe39b79725fb0ca0a7
wangjianlong <810844480@qq.com>
2015-02-09 22:12:06 +0800

    commit

--- loowootech.AtlasWeb/loowootech.AtlasWeb/Controllers/PictureController.cs
+++ loowootech.AtlasWeb/loowootech.AtlasWeb/Controllers/PictureController.cs
@@ -1,6 +1,8 @@
-﻿using loowootech.AtlasWeb.Web;
+﻿using loowootech.AtlasWeb.Helper;
+using loowootech.AtlasWeb.Web;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,10 +12,33 @@ namespace loowootech.AtlasWeb.Controllers
     [UserAuthorize]
     public class PictureController : ControllerBase
     {
-        public ActionResult Get(int ID, string Name) {
-            
+        public ActionResult Show(string Layer,int ID) {
+            string directory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Img/Data/",Layer,ID.ToString());
+            ViewBag.PList = Picture.GetPictures(directory, Layer, ID);
+            ViewBag.Layer = Layer;
+            ViewBag.ID = ID;
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Upload(string Layer,int ID) {
+            var file = Picture.GetPostFile(HttpContext);
+            if (!Picture.VerificationByExt(file.FileName)) {
+                throw new ArgumentException("目前不支持上传该格式图片");
+            }
+            Picture.Upload(file,Layer,ID);
+            return RedirectToAction("Show", new { Layer, ID });
+        }
+
+
+        [HttpPost]
+        public ActionResult Delete(string src,string Layer,int ID) {
+            if (!Picture.Delete(src, Layer, ID)) {
+                throw new ArgumentException("删除当前图片不存在或者当前没有照片！");
+            }
+            return RedirectToAction("Show", new { Layer,ID});
+        }
+
 
     }
 }
